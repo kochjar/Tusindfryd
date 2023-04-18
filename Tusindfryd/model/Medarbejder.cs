d62e91e4046671d6d098e6cf3569cc6fa91e7e9b
kochjar <timgfx@outlook.com>
2023-04-18 12:18:51 +0200

    Model done

--- Tusindfryd/model/Medarbejder.cs
+++ Tusindfryd/model/Medarbejder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,17 @@ namespace Tusindfryd.model
 {
     public class Medarbejder
     {
+        private string initialer;
+        private string navn;
+
+        public string Initialer { get { return initialer; } set { initialer = value; } }
+        public string Navn { get { return navn; } set { navn = value; } }
+
+        public Medarbejder(string initialer, string navn) {
+            
+            this.Navn = navn;
+            this.Initialer = initialer;
+            
+        }
     }
 }
