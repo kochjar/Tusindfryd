47ea87b8382e9ca88677c81392f0f124246fa0da
kochjar <timgfx@outlook.com>
2023-04-18 14:33:49 +0200

    Controller v1

---
+++ Tusindfryd/controller/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tusindfryd.controller
+{
+    public class Controller
+    {
+        private DrivhusRepository drivhusRepository;
+        private MedarbejderRepository medarbejderRepository;
+
+        public Controller() { 
+            this.medarbejderRepository = new MedarbejderRepository();
+            this.drivhusRepository = new DrivhusRepository(medarbejderRepository);
+        }
+    }
+}
